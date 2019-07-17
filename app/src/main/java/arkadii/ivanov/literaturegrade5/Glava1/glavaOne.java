702f9ebdbdf7f7a0580be9fc749a3bbfa64aa564
vasia18 <avi26@mail;.ru>
2019-07-17 17:43:54 +0300

    Добавил Glava1

---
+++ app/src/main/java/arkadii/ivanov/literaturegrade5/Glava1/glavaOne.java
@@ -0,0 +1,17 @@
+package arkadii.ivanov.literaturegrade5.Glava1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import arkadii.ivanov.literaturegrade5.R;
+
+public class glavaOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.null);
+    }
+}
